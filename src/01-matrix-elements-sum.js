f2bf4de328253be80ab75c513df2d07abad9f6fe
alexboagreek <olivesargreek@gmail.com>
2021-04-08 08:42:11 +0300

    modified: src/01-matrix-elements-sum.js

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -15,15 +15,18 @@
  * The result should be 9
  */
 function getMatrixElementsSum(matrix) {
-  let sumArr = null;
-  for (let i = 0; i < matrix.length; i++){
-    for ( let j = 0; j < matrix[0].length; j++){
-      if (i === 0 || matrix[i - 1][j] !== 0){
-        sumArr += matrix[i][j];
+  const extras = [];
+  let result = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        extras.push(j);
+      }
+      if (!extras.includes(j)) {
+        result += matrix[i][j];
       }
     }
   }
-  return sumArr;
+  return result;
 }
-
-module.exports = getMatrixElementsSum;
+export default getMatrixElementsSum;
